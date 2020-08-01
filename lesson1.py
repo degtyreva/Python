f9aefe3a81f5ceea06b6bb84cd5773ce8cc59267
degtyreva <68245025+degtyreva@users.noreply.github.com>
2020-08-01 15:38:36 +0200

    Add files via upload
    new test

---
+++ lesson1.py
@@ -0,0 +1,90 @@
+# print('Hello world!')  # это вывод в консоль
+#
+# print(5 + 5)
+# print(5 - 5)
+# print(5 / 5)
+# print(5 * 5)
+# print(10 // 3)
+# print(10 % 3)
+# print(10 ** 3)
+
+# my_int = 500
+# print(type(my_int))
+# my_int = my_int / 3
+# print(type(my_int))
+# print(a % 432)
+# print(a % 432)
+
+# Целое число
+# my_int = 10
+# print(type(my_int))
+
+# Дробные числа
+# my_float = 1.23
+# print(type(my_float))
+
+# Булевые
+# my_bool = False
+# print(my_bool)
+# print(type(my_bool))
+
+# print(int(float('1.6')))
+
+# print(bool(None))
+
+# a = 9
+# b = 9
+# c = 13
+# print((a > b) and (b < c))
+# print(not ((a > b) or (b < c) or (a > 5)))
+#
+# a = input('Введите текст: ')
+# print(a)
+
+# real_pass = 'qwerty'
+# real_admin_pass = '123'
+# p = input('Введите пароль: ')
+# if p == real_pass:
+#     print('Доступ предоставлен!')
+#     admin_p = input('Админский пароль: ')
+#     if real_admin_pass == admin_p:
+#         print('rooted!')
+# else:
+#     print('Неверный пароль!')
+
+
+# color = 'red'
+#
+# if color == 'red':
+#     print('Красный')
+# elif color == 'blue':
+#     print('Синий')
+# elif color == 'white':
+#     print('Белый')
+# else:
+#     print('Другое')
+
+# number = int(input('Введите число от 0 до 9: ')) # 55
+#
+# while number >= 10 or number < 0:
+#     number = int(input('Введите число от 0 до 9: '))
+# print('Ты справился с задачей')
+
+# i = 1
+# while i > 0:
+#     i += 1  # i = i + 1
+#     if i == 7:
+#         continue
+#     print(i)
+#     if i == 10:
+#         break
+
+
+# name = 'Максим'
+# surname = 'Иванов'
+# age = 25
+#
+# print(name, surname, age)
+# print('Имя: %s Фамилия: %s Возраст: %d' % (name, surname, age))
+# print('Имя: {} Фамилия: {} Возраст: {}'.format(name, surname, age))
+# print(f'Имя: {name} Фамилия: {surname} Возраст: {age}')
